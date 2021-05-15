b86093e8471dba3e6208bda0e19c72004fa2fbcc
MiguelFC99 <is715460@iteso.mx>
2021-05-14 21:41:45 -0500

    environment

--- frondendangular/src/environments/environment.prod.ts
+++ frondendangular/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://flights-info-api.herokuapp.com/',
   socketUrl: 'https://flights-info-api.herokuapp.com/',
+  clientId: '1081650056929-l0h1r2u31krm4nmjbkonsgk2gn76oo86.apps.googleusercontent.com',
+  clientSecret: 'wReglhu34UYYyH7NuYgF-p7dwReglhu34UYYyH7NuYgF-p7d'
 };
